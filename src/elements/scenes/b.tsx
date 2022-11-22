ac76bbe29dabb838e0e8236c01a6cdc3ce4dc9de
Iztecteocuitlatl <dongze.yi@zjmlkj.cn>
2022-11-22 16:04:41 +0800

    ray intersection detect example

--- src/elements/scenes/b.tsx
+++ src/elements/scenes/b.tsx
@@ -1,5 +1,16 @@
 import React, {useEffect, useRef} from "react";
-import {ArcRotateCamera, Engine, HemisphericLight, MeshBuilder, Scene, Vector3} from "@babylonjs/core";
+import {
+	AbstractMesh,
+	ArcRotateCamera, AxesViewer, Axis, Color3,
+	Engine,
+	HemisphericLight,
+	Material,
+	MeshBuilder, Ray, RayHelper,
+	Scene,
+	StandardMaterial, TransformNode,
+	Vector3
+} from "@babylonjs/core";
+import {Debug} from "@babylonjs/core/Legacy/legacy";
 
 const SceneB: React.FC = () => {
 	const reactCanvas = useRef(null)
@@ -27,6 +38,42 @@ const SceneB: React.FC = () => {
 		
 		return {camera};
 	}
+	const setUpScene = (scene: Scene) => {
+		// Create a box that always spinning
+		const spinBox = MeshBuilder.CreateBox('spin-box', {size: 1}, scene)
+		spinBox.position.set(3, 2, 0)
+		const spinBoxMaterial = new StandardMaterial('spin-box-material', scene)
+		spinBoxMaterial.diffuseColor = new Color3(0.24, 1, 0)
+		spinBoxMaterial.alpha = 0.5
+		spinBox.material = spinBoxMaterial
+		const spinBoxAxes = new AxesViewer(scene, 1)
+		spinBoxAxes.xAxis.parent = spinBox
+		spinBoxAxes.yAxis.parent = spinBox
+		spinBoxAxes.zAxis.parent = spinBox
+		scene.registerBeforeRender(() => {
+			spinBox.rotate(Axis.Y, 0.02)
+		})
+		
+		//
+		const targetCylinder = MeshBuilder.CreateCylinder('target-cylinder', {height: 1, diameter: 3}, scene)
+		targetCylinder.position.set(-3, 2, 0)
+		targetCylinder.material = spinBoxMaterial
+		
+		// Add a ray to the box, and detect
+		scene.registerBeforeRender(() => {
+			const ray = new Ray(Vector3.Zero(), Vector3.Zero(), 3.5)
+			const rayHelper = new RayHelper(ray)
+			rayHelper.attachToMesh(spinBox, new Vector3(1, 0, 1))
+			rayHelper.show(scene)
+			const interval = setInterval(() => {
+				rayHelper.dispose()
+				clearInterval(interval)
+			}, 100)
+			
+			console.log(ray.intersectsMesh(targetCylinder).hit)
+		})
+		
+	}
 	
 	useEffect(() => {
 		const {current: canvas} = reactCanvas
@@ -42,16 +89,16 @@ const SceneB: React.FC = () => {
 			{height: 20, width: 20, subdivisions: 4},
 			scene
 		);
-		const box = MeshBuilder.CreateBox('box', {size: 2}, scene);
-		const ball = MeshBuilder.CreateSphere(
-			'ball',
-			{segments: 8, diameter: 2},
-			scene
-		);
-		ball.position.set(0, 1, 0);
-		camera.setTarget(ball);
+		// const box = MeshBuilder.CreateBox('box', {size: 2}, scene);
+		// const ball = MeshBuilder.CreateSphere(
+		// 	'ball',
+		// 	{segments: 8, diameter: 2},
+		// 	scene
+		// );
+		// ball.position.set(0, 1, 0);
+		camera.setTarget(Vector3.Zero());
 		
-		
+		setUpScene(scene)
 		
 		const resize = () => {
 			scene.getEngine().resize();
